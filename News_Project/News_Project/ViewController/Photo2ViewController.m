8a203ede7d8caef705bfd284d3488a93727d8c26
hanhoo <hanhoo@hanhoodeMacBook-Pro.local>
2015-08-22 00:29:16 +0800

    01.hehe

---
+++ News_Project/News_Project/ViewController/Photo2ViewController.m
@@ -0,0 +1,37 @@
+//
+//  Photo2ViewController.m
+//  News_Project
+//
+//  Created by MS on 15/8/20.
+//  Copyright (c) 2015年 hanhu. All rights reserved.
+//
+
+#import "Photo2ViewController.h"
+#define PHOTO2_URL @"http://lifeweeker3.cms.palmtrends.com/api_v2.php?action=picture&gid=1790&offset=0&count=1000&uid=13836621"
+@interface Photo2ViewController ()
+
+@end
+
+@implementation Photo2ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
